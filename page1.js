f60e1d8ef15e053e1d7c43d22f9bf88e2dd6a2dc
Filip <101202559+ClanyX@users.noreply.github.com>
2025-01-23 17:55:15 +0100

    fix and complete ID creator

--- page1.js
+++ page1.js
@@ -1,13 +1,34 @@
-const username = document.getElementById('username').value;
-const lastname = document.getElementById('lastname').value;
-const age = document.getElementById('age').value;
-const gender = document.querySelector('input[name="gender"]:checked').value;
-
 const output = document.getElementById('personId');
+const personAray = [];
 
 function setNewID(){
-    console.log(username, lastname, age, gender);
-    output.innerHTML = `Name: ${username} Lastname: ${lastname} Age: ${age} Gender: ${gender}`;
+    const username = (document.getElementById('username').value).trim();
+    const lastname = (document.getElementById('lastname').value).trim();
+    const age = document.getElementById('age').value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+
+    const person = {
+        id: personAray.length + 1,
+        username : username,
+        lastname : lastname,
+        age: age,
+        gender: gender,
+    };
+    personAray.push(person);
+
+    document.getElementById('username').value = '';
+    document.getElementById('lastname').value = '';
+    document.getElementById('age').value = '';
+    document.querySelector('input[name="gender"]:checked').checked = false;
+
+    let genderAssci = '';
+    for(let i = 0; i < person.gender.length; i++){
+        const genderCode = `${gender.charCodeAt(i)}`;
+        genderAssci += genderCode;
+    }
+
+    const outputText = `${(person.age * 1e-3 + person.id).toString(8)}##${(person.username).split('').reverse().join('')}##${(person.lastname).split('').reverse().join('')}##${genderAssci}`;
+    output.innerHTML = outputText;
 }
 
 document.getElementById('submitBtn').addEventListener('submit', function(event) {
