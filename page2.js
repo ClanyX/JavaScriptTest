6e3e20fd3ab24a171f51c0337420bd481c4937cf
Filip <101202559+ClanyX@users.noreply.github.com>
2025-01-26 16:17:04 +0100

    edit some thinks in WS

--- page2.js
+++ page2.js
@@ -1,12 +1,25 @@
 const alfabet = 'abcdefghijklmnopqrstuvwxyz';
-let alfabetArray = alfabet.split('');
-let randomLetter = alfabetArray[Math.floor(Math.random() * alfabetArray.length)];
+const alfabetArray = alfabet.split('');
 
 
-fetch(`https://api.datamuse.com/words?sp=${randomLetter}*`)
+function getWord(){
+    let randomLetter = alfabetArray[Math.floor(Math.random() * alfabetArray.length)];
+    fetch(`https://api.datamuse.com/words?sp=${randomLetter}*`)
     .then(response => response.json())
     .then(values => {
         const randomWord = values[Math.floor(Math.random() * values.length)].word;
-        console.log(randomWord);
+        //must be edited
+        sendWord(randomWord);
     })
     .catch(error => console.error(error));
+} 
+
+//rename and complete this function
+function sendWord(word){
+    document.getElementById('outputText').innerHTML = word;
+}
+
+document.getElementById('sendInput').addEventListener('submit', function(event) {
+    event.preventDefault();
+    getWord();
+});
